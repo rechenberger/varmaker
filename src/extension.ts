2935e54226e47844be94af7fd8e295e66e9f5f19
Tristan Rechenberger <rechenberger@me.com>
2017-04-06 19:25:52 +0200

    working

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 'use strict';
 import * as vscode from 'vscode';
+import { Selection } from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -11,10 +12,30 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
         const selection = editor.selection;
-        const text = editor.document.getText(selection);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Selected text: ' + text);
+        editor.edit(edit => {
+            const startOfLine = selection.start.with(undefined, 0);
+            const line = startOfLine.line;
+            const selectedText = editor.document.getText(selection);
+            const parts = [
+                "const ",
+                "newVar",
+                " = ",
+                selectedText,
+                ";\r\n"
+            ]
+            edit.replace(startOfLine, parts.join(''));
+
+            edit.replace(selection, parts[1])
+
+            process.nextTick(() => {
+                editor.selections = [
+                    new Selection(selection.start.translate(1), selection.start.translate(1, parts[1].length)),
+                    new Selection(line, parts[0].length, line, parts[0].length + parts[1].length)
+                ]
+            })
+
+        });
     });
 
     context.subscriptions.push(disposable);
