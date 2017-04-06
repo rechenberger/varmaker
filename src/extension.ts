2fad799d36e81e9684040a6fbfbe9759dd570bc4
Tristan Rechenberger <rechenberger@me.com>
2017-04-06 18:08:32 +0200

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -3,29 +3,21 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    console.log('Congratulations, your extension "varmaker" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        vscode.window.showInformationMessage('Hello World..........!!!!');
-    });
-
-    let disposable2 = vscode.commands.registerCommand('extension.makeVar', () => {
-        var editor = vscode.window.activeTextEditor;
+    let disposable = vscode.commands.registerCommand('extension.makeVar', () => {
+        const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return; // No open text editor
         }
 
-        var selection = editor.selection;
-        var text = editor.document.getText(selection);
+        const selection = editor.selection;
+        const text = editor.document.getText(selection);
 
         // Display a message box to the user
         vscode.window.showInformationMessage('Selected text: ' + text);
     });
 
-    context.subscriptions.push(disposable, disposable2);
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
